40a7d7e67c498aa393bb036fc6450d2cd80f8a94
Ashfakurasif-code <ashfakurasif@gmail.com>
2026-05-31 19:37:48 +0200

    fix: all aw-firestore refs replaced

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ const PRECACHE = [
   '/css/joaf.css',
   '/js/data.js',
   '/js/components.js',
-  '/js/aw-firestore.js',
+  '/js/appwrite-db.js',
   '/js/main.js',
   '/logoc7c3.png',
   '/offline.html',
